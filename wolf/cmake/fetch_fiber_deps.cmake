95f07a05307d382b11bc893e7002b0e66d5a9e73
PooyaEimandar <pooya.eimandar@gmail.com>
2022-04-24 21:55:50 +0430

    initial commit

---
+++ wolf/cmake/fetch_fiber_deps.cmake
@@ -0,0 +1,412 @@
+if (WOLF_ENABLE_FIBER)
+    message("fetching https://github.com/boostorg/assert.git")
+        FetchContent_Declare(
+        boost_assert
+        GIT_REPOSITORY https://github.com/boostorg/assert.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/config.git")
+        FetchContent_Declare(
+        boost_config
+        GIT_REPOSITORY https://github.com/boostorg/config.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/context.git")
+        FetchContent_Declare(
+        boost_context
+        GIT_REPOSITORY https://github.com/boostorg/context.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/core.git")
+        FetchContent_Declare(
+        boost_core
+        GIT_REPOSITORY https://github.com/boostorg/core.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/smart_ptr.git")
+        FetchContent_Declare(
+        boost_smart_ptr
+        GIT_REPOSITORY https://github.com/boostorg/smart_ptr.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/mp11.git")
+        FetchContent_Declare(
+        boost_mp11
+        GIT_REPOSITORY https://github.com/boostorg/mp11.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/static_assert.git")
+        FetchContent_Declare(
+        boost_static_assert
+        GIT_REPOSITORY https://github.com/boostorg/static_assert.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/move.git")
+        FetchContent_Declare(
+        boost_move
+        GIT_REPOSITORY https://github.com/boostorg/move.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/intrusive.git")
+        FetchContent_Declare(
+        boost_intrusive
+        GIT_REPOSITORY https://github.com/boostorg/intrusive.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/algorithm.git")
+        FetchContent_Declare(
+        boost_algorithm
+        GIT_REPOSITORY https://github.com/boostorg/algorithm.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/pool.git")
+        FetchContent_Declare(
+        boost_pool
+        GIT_REPOSITORY https://github.com/boostorg/pool.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/throw_exception.git")
+        FetchContent_Declare(
+        boost_throw_exception
+        GIT_REPOSITORY https://github.com/boostorg/throw_exception.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/container_hash.git")
+        FetchContent_Declare(
+        boost_container_hash
+        GIT_REPOSITORY https://github.com/boostorg/container_hash.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/array.git")
+        FetchContent_Declare(
+        boost_array
+        GIT_REPOSITORY https://github.com/boostorg/array.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/predef.git")
+        FetchContent_Declare(
+        boost_predef
+        GIT_REPOSITORY https://github.com/boostorg/predef.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/filesystem.git")
+        FetchContent_Declare(
+        boost_filesystem
+        GIT_REPOSITORY https://github.com/boostorg/filesystem.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/type_traits.git")
+        FetchContent_Declare(
+        boost_type_traits
+        GIT_REPOSITORY https://github.com/boostorg/type_traits.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/bind.git")
+        FetchContent_Declare(
+        boost_bind
+        GIT_REPOSITORY https://github.com/boostorg/bind.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/integer.git")
+        FetchContent_Declare(
+        boost_integer
+        GIT_REPOSITORY https://github.com/boostorg/integer.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/detail.git")
+        FetchContent_Declare(
+        boost_detail
+        GIT_REPOSITORY https://github.com/boostorg/detail.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/format.git")
+        FetchContent_Declare(
+        boost_format
+        GIT_REPOSITORY https://github.com/boostorg/format.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/concept_check.git")
+        FetchContent_Declare(
+        boost_concept_check
+        GIT_REPOSITORY https://github.com/boostorg/concept_check.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/io.git")
+        FetchContent_Declare(
+        boost_io
+        GIT_REPOSITORY https://github.com/boostorg/io.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/winapi.git")
+        FetchContent_Declare(
+        boost_winapi
+        GIT_REPOSITORY https://github.com/boostorg/winapi.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/optional.git")
+        FetchContent_Declare(
+        boost_optional
+        GIT_REPOSITORY https://github.com/boostorg/optional.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/exception.git")
+        FetchContent_Declare(
+        boost_exception
+        GIT_REPOSITORY https://github.com/boostorg/exception.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/iterator.git")
+        FetchContent_Declare(
+        boost_iterator
+        GIT_REPOSITORY https://github.com/boostorg/iterator.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/utility.git")
+        FetchContent_Declare(
+        boost_utility
+        GIT_REPOSITORY https://github.com/boostorg/utility.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/function.git")
+        FetchContent_Declare(
+        boost_function
+        GIT_REPOSITORY https://github.com/boostorg/function.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/system.git")
+        FetchContent_Declare(
+        boost_system
+        GIT_REPOSITORY https://github.com/boostorg/system.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/tuple.git")
+        FetchContent_Declare(
+        boost_tuple
+        GIT_REPOSITORY https://github.com/boostorg/tuple.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/conversion.git")
+        FetchContent_Declare(
+        boost_conversion
+        GIT_REPOSITORY https://github.com/boostorg/conversion.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/mpl.git")
+        FetchContent_Declare(
+        boost_mpl
+        GIT_REPOSITORY https://github.com/boostorg/mpl.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/atomic.git")
+        FetchContent_Declare(
+        boost_atomic
+        GIT_REPOSITORY https://github.com/boostorg/atomic.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/function_types.git")
+        FetchContent_Declare(
+        boost_function_types
+        GIT_REPOSITORY https://github.com/boostorg/function_types.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/type_index.git")
+        FetchContent_Declare(
+        boost_type_index
+        GIT_REPOSITORY https://github.com/boostorg/type_index.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/variant2.git")
+        FetchContent_Declare(
+        boost_variant2
+        GIT_REPOSITORY https://github.com/boostorg/variant2.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/typeof.git")
+        FetchContent_Declare(
+        boost_typeof
+        GIT_REPOSITORY https://github.com/boostorg/typeof.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/align.git")
+        FetchContent_Declare(
+        boost_align
+        GIT_REPOSITORY https://github.com/boostorg/align.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/fusion.git")
+        FetchContent_Declare(
+        boost_fusion
+        GIT_REPOSITORY https://github.com/boostorg/fusion.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/range.git")
+        FetchContent_Declare(
+        boost_range
+        GIT_REPOSITORY https://github.com/boostorg/range.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/regex.git")
+        FetchContent_Declare(
+        boost_regex
+        GIT_REPOSITORY https://github.com/boostorg/regex.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/unordered.git")
+        FetchContent_Declare(
+        boost_unordered
+        GIT_REPOSITORY https://github.com/boostorg/unordered.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/container.git")
+        FetchContent_Declare(
+        boost_container
+        GIT_REPOSITORY https://github.com/boostorg/container.git
+        GIT_TAG        master
+    )
+    message("fetching https://github.com/boostorg/fiber.git")
+        FetchContent_Declare(
+        boost_fiber
+        GIT_REPOSITORY https://github.com/boostorg/fiber.git
+        GIT_TAG        master
+    )
+    # make them available
+    FetchContent_MakeAvailable(boost_assert)
+    FetchContent_MakeAvailable(boost_config)
+    FetchContent_MakeAvailable(boost_context)
+    FetchContent_MakeAvailable(boost_core)
+    FetchContent_MakeAvailable(boost_smart_ptr)
+    FetchContent_MakeAvailable(boost_mp11)
+    FetchContent_MakeAvailable(boost_static_assert)
+    FetchContent_MakeAvailable(boost_move)
+    FetchContent_MakeAvailable(boost_intrusive)
+    FetchContent_MakeAvailable(boost_algorithm)
+    FetchContent_MakeAvailable(boost_pool)
+    FetchContent_MakeAvailable(boost_throw_exception)
+    FetchContent_MakeAvailable(boost_container_hash)
+    FetchContent_MakeAvailable(boost_array)
+    FetchContent_MakeAvailable(boost_predef)
+    FetchContent_MakeAvailable(boost_filesystem)
+    FetchContent_MakeAvailable(boost_type_traits)
+    FetchContent_MakeAvailable(boost_bind)
+    FetchContent_MakeAvailable(boost_integer)
+    FetchContent_MakeAvailable(boost_detail)
+    FetchContent_MakeAvailable(boost_format)
+    FetchContent_MakeAvailable(boost_concept_check)
+    FetchContent_MakeAvailable(boost_winapi)
+    FetchContent_MakeAvailable(boost_io)
+    FetchContent_MakeAvailable(boost_optional)
+    FetchContent_MakeAvailable(boost_exception)
+    FetchContent_MakeAvailable(boost_iterator)
+    FetchContent_MakeAvailable(boost_utility)
+    FetchContent_MakeAvailable(boost_function)
+    FetchContent_MakeAvailable(boost_system)
+    FetchContent_MakeAvailable(boost_tuple)
+    FetchContent_MakeAvailable(boost_conversion)
+    FetchContent_MakeAvailable(boost_mpl)
+    FetchContent_MakeAvailable(boost_atomic)
+    FetchContent_MakeAvailable(boost_function_types)
+    FetchContent_MakeAvailable(boost_type_index)
+    FetchContent_MakeAvailable(boost_variant2)
+    FetchContent_MakeAvailable(boost_typeof)
+    FetchContent_MakeAvailable(boost_align)
+    FetchContent_MakeAvailable(boost_fusion)
+    FetchContent_MakeAvailable(boost_range)
+    FetchContent_MakeAvailable(boost_regex)
+    FetchContent_MakeAvailable(boost_unordered)
+    FetchContent_MakeAvailable(boost_container)
+    FetchContent_MakeAvailable(boost_fiber)
+
+# add includes
+list(APPEND INCLUDES
+    ${boost_assert_SOURCE_DIR}/include
+    ${boost_config_SOURCE_DIR}/include
+    ${boost_context_SOURCE_DIR}/include
+    ${boost_core_SOURCE_DIR}/include
+    ${boost_smart_ptr_SOURCE_DIR}/include
+    ${boost_mp11_SOURCE_DIR}/include
+    ${boost_static_assert_SOURCE_DIR}/include
+    ${boost_move_SOURCE_DIR}/include
+    ${boost_intrusive_SOURCE_DIR}/include
+    ${boost_algorithm_SOURCE_DIR}/include
+    ${boost_pool_SOURCE_DIR}/include
+    ${boost_throw_exception_SOURCE_DIR}/include
+    ${boost_container_hash_SOURCE_DIR}/include
+    ${boost_array_SOURCE_DIR}/include
+    ${boost_predef_SOURCE_DIR}/include
+    ${boost_filesystem_SOURCE_DIR}/include
+    ${boost_type_traits_SOURCE_DIR}/include
+    ${boost_bind_SOURCE_DIR}/include
+    ${boost_integer_SOURCE_DIR}/include
+    ${boost_detail_SOURCE_DIR}/include
+    ${boost_format_SOURCE_DIR}/include
+    ${boost_concept_check_SOURCE_DIR}/include
+    ${boost_winapi_SOURCE_DIR}/include
+    ${boost_io_SOURCE_DIR}/include
+    ${boost_optional_SOURCE_DIR}/include
+    ${boost_exception_SOURCE_DIR}/include
+    ${boost_iterator_SOURCE_DIR}/include
+    ${boost_utility_SOURCE_DIR}/include
+    ${boost_function_SOURCE_DIR}/include
+    ${boost_system_SOURCE_DIR}/include
+    ${boost_tuple_SOURCE_DIR}/include
+    ${boost_conversion_SOURCE_DIR}/include
+    ${boost_mpl_SOURCE_DIR}/include
+    ${boost_atomic_SOURCE_DIR}/include
+    ${boost_function_types_SOURCE_DIR}/include
+    ${boost_type_index_SOURCE_DIR}/include
+    ${boost_variant2_SOURCE_DIR}/include
+    ${boost_typeof_SOURCE_DIR}/include
+    ${boost_align_SOURCE_DIR}/include
+    ${boost_fusion_SOURCE_DIR}/include
+    ${boost_range_SOURCE_DIR}/include
+    ${boost_regex_SOURCE_DIR}/include
+    ${boost_unordered_SOURCE_DIR}/include
+    ${boost_container_SOURCE_DIR}/include
+    ${boost_fiber_SOURCE_DIR}/include
+)
+
+# add libraries
+list(APPEND LIBS 
+    boost_assert 
+    boost_config
+    boost_context 
+    boost_core
+    boost_smart_ptr
+    boost_mp11
+    boost_static_assert
+    boost_move
+    boost_intrusive
+    boost_algorithm
+    boost_pool
+    boost_throw_exception
+    boost_container_hash
+    boost_array
+    boost_predef
+    boost_filesystem
+    boost_type_traits
+    boost_bind
+    boost_integer
+    boost_detail
+    boost_format
+    boost_concept_check
+    boost_winapi
+    boost_io
+    boost_optional
+    boost_exception
+    boost_iterator
+    boost_utility
+    boost_mpl
+    boost_atomic
+    boost_function_types
+    boost_type_index
+    boost_variant2
+    boost_align
+    boost_fusion
+    boost_range
+    boost_regex
+    boost_unordered
+    boost_container
+    boost_fiber
+)
+
+endif()
